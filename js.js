0e7a58d119b7d953e128e933c259de481861691a
Alexey205 <sergsergsaa@gmail.com>
2022-06-27 11:23:37 +0300

    adding cards

--- js.js
+++ js.js
@@ -1,8 +1,27 @@
 const url = "https://my-json-server.typicode.com/Alexey205/MartPlace/products"
+const cards = document.getElementById("cards");
+const container = document.getElementById("container");
 
 $.ajax({
     url: url,
     success: function(result){
         console.log(result);
+
+	    result.forEach(result => {
+	        // div.classList.add('card');
+		    let div = `
+            <div class="card">
+                <h2><b>${result.name}</b></h2>
+                <img src="${result.photo_url}" heigth="100px" width="100px"></img>
+                <p><b>Price:</b> ${result.price} грн.</p>
+                <p><b>Description:</b> ${result.description}</p>
+                <br>
+                <a href="#">Seller profile</a><br><br>
+                <button class="buybtn">Buy</button>
+            </div>
+		    `;
+            cards.innerHTML += div;
+	    });
+    container.appendChild(cards);
     }
 });
